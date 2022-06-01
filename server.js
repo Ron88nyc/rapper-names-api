170b5cab68b5b94201f76f266e3a081edfbb93fa
Ronald Li <Ronaldnyc88@gmail.com>
2022-06-01 15:09:54 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const PORT = 8000
+
+app.use(cors())
+const rappers = {
+    '21 savage': {
+        'age':29,
+    'birthName':'Sheyaa Bin Abraham-Joseph',
+    'birthLocation': 'London, England'
+    },
+    'chance The Rapper': {
+        'age':29,
+    'birthName':'Chancelor Bennet',
+    'birthLocation': 'Chicago, Illinois'
+    },
+    'dylan': {
+        'age':29,
+    'birthName':'Dylan',
+    'birthLocation': 'Dylan'
+    }
+}
+
+
+app.get('/',(request, response)=>{
+    response.sendFile(__dirname + '/index.html')
+}) 
+
+app.get('/api/:rapperName', (request,response)=> {
+    const rappersName = request.params.rapperName.toLowerCase()
+    if(rappers[rappersName]){
+        response.json(rappers[rappersName])
+    }else {
+        response.json(rappers['Dylan'])
+    }
+    // response.json(rappers)
+})
+
+
+app.listen(process.env.PORT || PORT, ()=> {
+    console.log(`The server is running on ${PORT}! You better go catch it!`)
+})
